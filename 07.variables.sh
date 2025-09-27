17094a4e083d8384f7bfcc4a98e6fac281cb21cc
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2025-09-27 21:29:03 +0530

    env

---
+++ 07.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#DATE=$(date +%s)
+
+START_TIME=$(date +%s)
+END_TIME=$(date +%s)
+TOTAL_TIME=$((END_TIME-START_TIME))
+
+echo "Taotal time exicute the scripts time is :$TOTAL_TIME seconds"
+echo "course name is: $COURSE"
