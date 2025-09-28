7eb98c83c83764430f398c229fb47e9b0f399876
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2025-09-28 17:35:10 +0530

    condition

--- 11.condition-practice.sh
+++ 11.condition-practice.sh
@@ -5,10 +5,12 @@ NUMBER=$1
 echo "please enter the number"
 read NUMBER
 
-if  [ $(($NUMBER % 1) -eq 0)]; then
+if  [ $(($NUMBER % 1) -eq 0) ]; then
     echo "Given number is equal to 0, So the Number is prime number"
-elif [ $(($NUMBER % $NUMBER) -eq 0)]; then
+elif [ $(($NUMBER % $NUMBER) -eq 0) ]; then
     echo "Given number is equal to 0, So the number is prime number"
+elif [ $(($NUMBER % 2)) -eq 0 ]; then
+    echo "Given number is equal to 0, So the number is non-prime number"
 else
     echo "Given number is not equal to 0, So the number is non-prime number"
 fi
