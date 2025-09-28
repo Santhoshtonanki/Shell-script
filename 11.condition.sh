d796b8a06c272c13f6780b775878691b7de73897
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2025-09-28 17:10:13 +0530

    condition

--- 11.condition.sh
+++ 11.condition.sh
@@ -4,7 +4,7 @@ echo "please enter the number"
 read NUMBER
 
 if [ $(($NUMBER % 2)) -eq 0 ]; then
-    echo "Given number is  EVEN number"
+    echo "Given number is equal to 0, the number is  EVEN number"
 else 
-    echo "Given number is ODD number"
+    echo "Given number is not equal to 0, the number is  ODD number"
 fi
