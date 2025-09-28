554013e80c9b09bb9da399a86b7e3c6c3225285f
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2025-09-28 17:21:44 +0530

    condition

---
+++ 11.condition-practice.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUMBER=$1
+
+echo "please enter the number"
+read NUMBER
+
+if  [ $(($NUMBER % 1) -eq 0)]; then
+    echo "Given number is equal to 0, So the Number is prime number"
+elif [ $(($NUMBER % $NUMBER) -eq 0)]; then
+    echo "Given number is equal to 0, So the number is prime number"
+else
+    echo "Given number is not equal to 0, So the number is not prime number"
+fi
