a6f325d006a2b18171021101d611aed4c3b1a5e0
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2025-09-28 11:22:45 +0530

    env

--- 10.conditions.sh
+++ 10.conditions.sh
@@ -4,13 +4,11 @@ NUBMER=$1
 
 if {[ $NUMBE -lt 10 ]then;
     echo "number $NUMBER is less than 10"
-}
-elif {
-    echo "number $NUMBER is greater than 10"
-}
 
-else {
+elif 
+    echo "number $NUMBER is greater than 10"
+
+else 
     echo "number $NUMBER is equal to 10"
-}
-    
+}  
 fi
