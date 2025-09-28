212ae7e5fb124f7b0c7f96d4c6944b607de628f5
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2025-09-28 23:12:38 +0530

    install mongosh

---
+++ 14.colour.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+B="\e[30m"
+R="\e[31m"
+G="\e[332m"
+Y="\e[33m"
+C="\e[36m"
+W="\e[37m"
+N="\e[0m"
+
+echo what are you doing? raju
+echo $B nothing? $N $G em chestunnavu nuvvu ramu? $N
+echo $Y edo cheptunnavu $N $G ramu $N
+echo $G ok $N
+echo ${R}red color${N}
