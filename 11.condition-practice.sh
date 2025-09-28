9f8e1ea6046a181649c5e3f473187764120ecab1
Santhoshtonanki <santhoshtonanki1996@gmail.com>
2025-09-28 17:38:45 +0530

    condition

--- 11.condition-practice.sh
+++ 11.condition-practice.sh
@@ -7,7 +7,7 @@ read NUMBER
 
 if  [ $(($NUMBER % 1) -eq 0) ]; then
     echo "Given number is equal to 0, So the Number is prime number"
-elif [ $(($NUMBER % $NUMBER) -eq 0) ]; then
+elif [ $(($NUMBER % $i) -eq 0) ]; then
     echo "Given number is equal to 0, So the number is prime number"
 elif [ $(($NUMBER % 2)) -eq 0 ]; then
     echo "Given number is equal to 0, So the number is non-prime number"
